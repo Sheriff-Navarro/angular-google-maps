4df83eb7f67466ea197a7f8313c82a2ad0038f56
Nick Navarro <jacinto802@gmail.com>
2017-11-08 19:24:26 -0500

    Angular Google Maps example, API key removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'My first AGM project';
+  lat: number = 27.678418;
+  lng: number = -81.809007;
 }
